9ad22966e135bb1ba55095137b2319271e64f2b8
Renan Bernardelli <renanbernardelli@gmail.com>
2022-03-06 13:43:31 +0000

    :truck: Bitcoin lottie moved to Lottie folder

--- frontend/src/components/minotaurLottieTitleLogo.js
+++
@@ -1,8 +0,0 @@
-import Lottie from "lottie-react";
-import minotaurLottieTitleLogo from "../assets/lotties/minotaur-lottie-title.json";
-
-const MinotaurLottieTitleLogo = () => {
-    return <Lottie animationData={minotaurLottieTitleLogo} loop={0} autoplay={true}></Lottie>
-}
-
-export default MinotaurLottieTitleLogo;
