1e2613b8b4e190477ead9feaf6588ed322c0326c
Renan Bernardelli <renanbernardelli@gmail.com>
2022-03-09 08:51:00 +0000

    :lock: add .env to hide the port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-app.listen(3001, () => {
-    
-    console.log('App is running.');
+app.listen(process.env.PORT, () => {
+
+    console.log('App is running at ' + process.env.PORT);
 })
