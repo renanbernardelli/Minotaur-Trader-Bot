ef87409f78eef1eb1fbe8898a64a1d80e58ed568
Renan Bernardelli <renanbernardelli@gmail.com>
2022-03-10 08:59:31 +0000

    :heavy_plus_sign: Add dependencies: :pencil2: Body Parser (express.json), :passport_control: Cors and Helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
 
 const app = express();
 
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
+
+app.use('/Minotaur', (rep, res, next) => {
+    res.send('Hello Minotaur!');
+});
+
 app.use('/', (req, res, next) => {
 
-    res.send('Hello world')
-})
+    res.send('Hello world');
+});
 
 module.exports = app;
