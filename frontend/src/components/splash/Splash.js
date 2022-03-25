8d6e6651b9e35c888400789f0f946adb5a5c5cfd
Renan Bernardelli <renanbernardelli@gmail.com>
2022-03-25 09:09:48 +0000

    :truck: Move to components folder. :recycle: Improve the code structure

---
+++ frontend/src/components/splash/Splash.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BitcoinLottieLogo from "../lotties/bitcoinLottieLogo";
+
+function Splash() {
+
+    return (
+
+        <div className='container login-splash'>
+            <BitcoinLottieLogo></BitcoinLottieLogo>
+        </div>
+    )
+};
+
+
+export default Splash;
