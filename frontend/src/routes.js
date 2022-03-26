edc6351a4802733551451ddd23336c5f3f6bf745
Renan Bernardelli <renanbernardelli@gmail.com>
2022-03-26 20:52:47 +0000

    :heavy_minus_sign: Remove Splash and organise the code

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,15 +2,12 @@ import React from "react";
 import { Route, BrowserRouter, Redirect } from "react-router-dom";
 import Login from "./public/Login/Login";
 import Settings from "./private/Settings/settings";
-import Splash from "./public/Splash/Splash";
 
 function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact>
-                <Login>
-
-                </Login>
+                <Login></Login>
             </Route>
 
             <Route path="/settings">
