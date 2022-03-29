06ef951a6b7ab90902c9c70f170ea5c06039c3c8
Renan Bernardelli <renanbernardelli@gmail.com>
2022-03-29 08:15:57 +0100

    :construction: Working on req and res

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,26 @@ app.use(helmet());
 
 app.use(express.json());
 
-app.use('/Minotaur', (rep, res, next) => {
-    res.send('Hello Minotaur!');
+app.post('/login', (req, res, next) => {
+
+    const email =  req.body.email;
+    const password = req.body.password;
+
+    if(email === 'test@test.com' && password === '123') {
+
+        res.status(200);
+        res.json('OK! Está dentro!')
+    }
+    else {
+        
+        res.status(401);
+        res.json('Error!');
+    }
+});
+
+app.post('/logout', (req, res, next) => {
+
+    res.status(200);
 });
 
 app.use('/', (req, res, next) => {
