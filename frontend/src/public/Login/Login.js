f754f087aa7c0a36b61ff38d2f7daf12e2799bbc
Renan Bernardelli <renanbernardelli@gmail.com>
2022-04-04 08:41:16 +0100

    :lipstick: Improve login style.

--- frontend/src/public/Login/Login.js
+++ frontend/src/public/Login/Login.js
@@ -75,7 +75,7 @@ function Login() {
                     </p> */}
                     <div className='col-12 d-flex align-items-center justify-content-center'>
                         
-                        <div className='col-12 bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500'>
+                        <div className='col-12 bg-white border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500 login-container'>
                             <div className='text-center logo-login'>
                                <div>
                                     <img alt="LaserEyes" src="../../../img/laserEyes.png" className="logo-laserEyes left"></img>
@@ -97,7 +97,7 @@ function Login() {
                             <form className='mt-4' action='#' onSubmit={OnSubmit}>
                                 <div className="form-group mb-4">
                                     <label htmlFor="email">Your Email</label>
-                                    <div className="input-group">
+                                    <div className="input-group input-group--login">
                                         <span className="input-group-text" id="basic-addon1">
                                             <svg className="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                                         </span>
