cc660b817c7456f4b2dd3d22985b7454554c2178
Renan Bernardelli <renanbernardelli@gmail.com>
2022-04-04 08:40:11 +0100

    :lock: Add private rotes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,15 +4,32 @@ import Login from "./public/Login/Login";
 import Settings from "./private/Settings/settings";
 
 function Routes() {
+
+    function PrivateRoute({ children, ...rest }) {
+
+        return (
+
+            <Route {...rest} render={() => {
+
+                return localStorage.getItem('token')
+                ? children
+                : <Redirect to="/"></Redirect>
+            }}></Route>
+        )
+    }
+
     return (
+
         <BrowserRouter>
+
             <Route path="/" exact>
                 <Login></Login>
             </Route>
 
-            <Route path="/settings">
+            <PrivateRoute path="/settings">
                 <Settings></Settings>
-            </Route>
+            </PrivateRoute>
+            
         </BrowserRouter>
     )
 }
