ac5aba7a6fc39e55982f42ed04e13a8442039dc6
Renan Bernardelli <renanbernardelli@gmail.com>
2022-04-07 09:01:46 +0100

    :construction: Add logout function and button. Import useEffect() and useState(). First token control to allow access to settings.

--- frontend/src/private/Settings/settings.js
+++ frontend/src/private/Settings/settings.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useHistory} from 'react-router-dom'
+import {getSettings} from '../../services/SettingsService';
+import {doLogout} from '../../services/AuthService';
 
 function Settings() {
+
+    const history = useHistory();
+    
+    const [error, setError] = useState('');
+
+    const [settings, setSettings] = useState({
+        email: '',
+        apiUrl:'',
+        accessKey:'',
+        keySecret:''
+    })
+
+    useEffect(() => {
+
+        const token = localStorage.getItem('token');
+
+        getSettings(token)
+            .then(response => {
+
+                setSettings(response);
+            })
+            .catch(err => {
+
+                if (err.response && err.response.status === 401) {
+
+                    return history.push('/');
+                }
+
+                if(err.response) {
+
+                    setError(err.response.data);
+                }
+                else {
+
+                    setError(err.message);
+                }
+            })
+    }, [])
+
+    function logoutOnClick(event) {
+
+        const token = localStorage.getItem('token');
+
+        doLogout(token)
+            .then(response => {
+                
+                localStorage.removeItem('token');
+                history.push('/');
+            })
+            .catch(err => {
+                setError(err.message);
+            })
+    }
+
     return (
         <main>
             <section className="vh-lg-100 mt-5 lg-0 bg-sort d-flex align-items-center">
@@ -9,8 +65,19 @@ function Settings() {
                     <p className='text-center'>
                         <Link to='/' className='d-flex align-items-center justify-content-center'>
                         <svg className="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path></svg>
-                        Settings
+
+                        {settings.email}
                         </Link>
+
+                        {
+                            error
+                            ?
+                            <div className='d-block text-center error-message mt-2'>{error}</div>
+                            :
+                            <React.Fragment></React.Fragment>
+                        }
+
+                        <button type='submit' className='btn btn-primary' onClick={logoutOnClick}>Logout</button> 
                     </p>
                 </div>
 
